32ae9294c91992421dfa56a5fd9feb2cff1faa53
Shady GOMAA <shady.gomaa@idemia.com>
2021-10-05 13:03:49 +0200

    add final close

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 //import com.mysql.cj.xdevapi.Statement;
 
 public class main {
-	
+
 	private static Connection conn;
 	private static Statement st;
 	private static ResultSet rs;
@@ -32,11 +32,10 @@ public class main {
 			String dbUser = "root";
 			String dbPassword = "root";
 //			Class.forName(myDriver);
-			
-			
+
 			// create our mysql database connection
 			conn = DriverManager.getConnection(myUrl, dbUser, dbPassword);
-			
+
 			// get data from mysql database
 			// our SQL SELECT query.
 			// if you only need a few columns, specify them by name instead of using "*"
@@ -44,28 +43,27 @@ public class main {
 
 			// create the java statement
 			st = conn.createStatement();
-			
-			
-		//insert into database
-			  //st.execute("insert into users values(3,\"khaled\",\"khaled\",\"khaled\")");
-		//update data in database
-			 //st.execute("update users set name='mohamed',user='mohamed',password='mohamed' where id=3");
-		//delete from database 
-			//st.execute("delete from users where id=2");
-			
+
+			// insert into database
+			// st.execute("insert into users values(3,\"khaled\",\"khaled\",\"khaled\")");
+			// update data in database
+			// st.execute("update users set name='mohamed',user='mohamed',password='mohamed'
+			// where id=3");
+			// delete from database
+			// st.execute("delete from users where id=2");
 
 			// execute the query, and get a java resultset
 			rs = st.executeQuery(query);
 
 //			// iterate through the java resultset
 			while (rs.next()) {
-			user user = new user();
-	        user.setId(rs.getInt("id"));
-	        user.setName(rs.getString("name"));
-	        user.setUser(rs.getString("user"));
-	        user.setPassword(rs.getString("password"));
-	        userList.add(user);
-	       
+				user user = new user();
+				user.setId(rs.getInt("id"));
+				user.setName(rs.getString("name"));
+				user.setUser(rs.getString("user"));
+				user.setPassword(rs.getString("password"));
+				userList.add(user);
+
 //				int id = rs.getInt("id");
 //				String name = rs.getString("name");
 //				String user = rs.getString("user");
@@ -77,11 +75,27 @@ public class main {
 		} catch (Exception e) {
 			System.err.println("Got an exception! ");
 			System.err.println(e.getMessage());
+		} finally {
+			try {
+
+				if (rs != null) {
+					rs.close();
+				}
+
+				if (st != null) {
+					st.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+			}
 		}
-		
-		//Check if data retreived successfully
-	       System.out.println(Integer.toString(userList.get(0).getId()));
-		
+
+		// Check if data retreived successfully
+		System.out.println(Integer.toString(userList.get(0).getId()));
+
 	}
 
 }
